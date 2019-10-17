7c0e628bdf3844ab3c75970849aca7a51c14a343
belohnung <projektarbeityt@gmail.com>
2019-10-17 19:17:39 +0200

    soos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::game::ConnectionState;
+use crate::game::{ConnectionState, Entity};
 use crate::packets::{player, pos};
 use crate::protocol::Packet;
 use mc_varint::{VarIntRead, VarIntWrite};
@@ -16,38 +16,43 @@ mod packets;
 mod protocol;
 
 fn main() {
-    for i in 0..50 {
-        thread::spawn(move || {
-            bot(format!("soos{}", i).parse().unwrap());
-        });
-    }
-    loop {}
+    bot("Nogga".parse().unwrap());
 }
 
 fn bot(name: String) {
     //  let mut loggedin = false;
-    let ipadress = "5.181.151.65";
+    let ipadress = "45.154.51.160";
     let port = 25565;
     let mut state = ConnectionState::Login;
+    let mut entity = Entity {
+        entityid: 0,
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+        yaw: 0.0,
+        pitch: 0.0,
+    };
     match TcpStream::connect(format!("{}:{}", ipadress, port)) {
         Ok(mut stream) => {
             let stream = Arc::new(Mutex::new(stream));
-            let mut x = 0.0;
+            let entity = Arc::new(Mutex::new(entity));
             {
                 let stream2 = stream.clone();
+                let entity2 = entity.clone();
                 thread::spawn(move || loop {
+                    thread::sleep(Duration::from_millis(50));
+                    let lockedentity = entity2.lock().unwrap();
                     Packet::ClientPlayerPositionAndLook {
-                        x: 0.545,
-                        y: 1.000000,
-                        z: 0.0,
-                        yaw: x,
-                        pitch: 0.0,
+                        x: lockedentity.x,
+                        y: lockedentity.y,
+                        z: lockedentity.z,
+                        yaw: lockedentity.yaw,
+                        pitch: lockedentity.pitch,
                         onground: false,
                     }
                     .serialize(&mut *stream2.lock().unwrap())
                     .unwrap();
-                    println!("tick  {}", x);
-                    x = x + 0.001;
+                    println!("tick !");
                 });
             }
 
@@ -82,6 +87,24 @@ fn bot(name: String) {
                         Packet::ClientKeepAlive { magic: moom }
                             .serialize(&mut *stream.lock().unwrap())
                             .unwrap();
+                        let mut locked = entity.lock().unwrap();
+                        locked.z += 1.0;
+                        locked.x += 1.0;
+                    }
+                    Packet::ServerPlayerPositionAndLook {
+                        x,
+                        y,
+                        z,
+                        yaw,
+                        pitch,
+                        flags,
+                    } => {
+                        let mut lockedentity = entity.lock().unwrap();
+                        lockedentity.x = x;
+                        lockedentity.y = y;
+                        lockedentity.z = z;
+                        lockedentity.yaw = yaw;
+                        lockedentity.pitch = pitch;
                     }
 
                     p => println!("packet lol xd: {:#?}", p),
